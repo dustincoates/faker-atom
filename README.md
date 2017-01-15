bdaeac5feb4b88a7b8a56e6d64ca24e81e5c5af1
Dustin Coates <dustin@dcoates.com>
2017-01-15 22:05:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# faker-atom
+
+An atom package using [faker.js](https://github.com/marak/Faker.js/) to pull in fake data to Atom.
+
+To use, write the path of what you want, e.g. `phone.phoneNumber` or `title.FirstName` then select it. Invoke `faker create` from the command palette or the right-click menu.
+
+If invoked without a selection, a full name will be inserted (`name.findName`).
---
+++ spec/faker-spec.js
@@ -0,0 +1,17 @@
+'use babel';
+
+import Faker from '../lib/faker';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('Faker', () => {
+  let workspaceElement, activationPromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('faker');
+  });
+});
